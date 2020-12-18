0211e7fd909aecadcb20bfa1c329d8f2b9f75e08
GitSkynet <carlos.curtido@gmail.com>
2020-12-18 10:37:42 +0100

    Deploy Heroku Main Page+

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c1274b88b5ff62e0dd71061034cd5e1.js"
+  "/precache-manifest.9074706766b7dc91384ee00aea2546c4.js"
 );
 
 self.addEventListener('message', (event) => {
